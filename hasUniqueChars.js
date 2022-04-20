27425205fbbac0a97e3480d24dd2bc67aa59ad24
tuli kinikini <tuli.kinikini@gmail.com>
2022-04-20 10:31:35 -0600

    uniqueChars done

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,19 @@
 // Write your code below
+
+
+
+function hasUniqueChars(word) {
+
+    let split = word.split('')
+    let returned = true
+    for(let i = 0; i < split.length; i++) {
+        for(let j = i+1; j < split.length; j++) {
+           if(split[i] === split[j]) {
+            return false
+           }
+        }
+    }
+    return returned
+}
+
+console.log(hasUniqueChars('Tuli'))
