82941d284f8124a781a218817db7133f3c1fcd3b
tuli kinikini <tuli.kinikini@gmail.com>
2022-04-20 10:46:37 -0600

    for in loop addition

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -6,7 +6,7 @@ function hasUniqueChars(word) {
 
     let split = word.split('')
     let returned = true
-    for(let i = 0; i < split.length; i++) {
+    for(let i in split) {
         for(let j = i+1; j < split.length; j++) {
            if(split[i] === split[j]) {
             return false
@@ -16,4 +16,4 @@ function hasUniqueChars(word) {
     return returned
 }
 
-console.log(hasUniqueChars('Tuli'))
+console.log(hasUniqueChars('Tult'))
